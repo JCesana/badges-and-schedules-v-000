1fb3e82d192014bf3d5c299a4ec085138fda091f
Josh Cesana <joshuacesana@gmail.com>
2018-05-17 23:38:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,19 @@
 # Write your code here.
+
 def batch_badge_creator(names)
   
+  names.each do |name|
+    "Hello, my name is #{name}."
+  end 
+  
+end 
+
+
+def assign_rooms(names)
+  
+end 
+
+
+def printer 
+
 end 
