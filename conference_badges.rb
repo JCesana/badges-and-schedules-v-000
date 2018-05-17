b4a72a48dbb6d48db12d5dc50066691575361883
Josh Cesana <joshuacesana@gmail.com>
2018-05-17 23:40:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,7 @@
 # Write your code here.
 
-def badge_maker(names)
-  
-  names.each do |name|
-    "Hello, my name is #{name}."
-  end 
-  
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end 
 
 
