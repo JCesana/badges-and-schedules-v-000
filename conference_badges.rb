a5ffb7128d959e5ce86aeda5350d882822273056
Josh Cesana <joshuacesana@gmail.com>
2018-05-18 00:08:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -29,7 +29,7 @@ def assign_rooms(names)
 end 
 
 
-def printer
-  puts batch_badge_creator(names)
-  puts assign_rooms(names)
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end 
